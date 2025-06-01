4c76822c74df4fe97d283d92ad6eea936f6aa233
ranselorm <ranselorm@outlook.com>
2025-06-01 10:30:27 +0000

    Install prisma

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import express from "express";
-const app = express();
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
