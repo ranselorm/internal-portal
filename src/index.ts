e8b560281bda5c0a4ad1e24d7c2518b70102bae0
ranselorm <ranselorm@outlook.com>
2025-06-01 17:01:41 +0000

    Fix prisma client error

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express, { Express, Request, Response } from "express";
 import { PORT } from "./secrets";
 import rootRouter from "./routes";
+import { PrismaClient } from "@prisma/client";
 
 const app: Express = express();
 
 app.use(express.json());
 
 app.use("/api", rootRouter);
+
+export const prismaClient = new PrismaClient();
 
 app.listen(PORT || 7071, () => {
   console.log(`App is running on port ${PORT}`);
