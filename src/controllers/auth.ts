3117c455ccacfeeb02db8fe13c89836185b50c19
ranselorm <ranselorm@outlook.com>
2025-06-01 17:55:48 +0000

    Login the user

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { prismaClient } from "..";
-import { hashSync } from "bcrypt";
+import { hashSync, compareSync } from "bcrypt";
+import * as jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../secrets";
 
 export const signup = async (req: Request, res: Response) => {
   const { email, password, name } = req.body;
@@ -22,6 +24,25 @@ export const signup = async (req: Request, res: Response) => {
   res.json(user);
 };
 
-export const login = (req: Request, res: Response) => {
-  res.send("Hit login routes");
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  let user = await prismaClient.user.findFirst({ where: { email } });
+
+  if (!user) {
+    throw Error("User does not exist");
+  }
+
+  if (!compareSync(password, user.password)) {
+    throw Error("Password is not correct");
+  }
+
+  const token = jwt.sign(
+    {
+      userId: user.id,
+    },
+    JWT_SECRET
+  );
+
+  res.json({ user, token });
 };
